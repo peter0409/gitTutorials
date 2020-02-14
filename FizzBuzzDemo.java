4fedc58a1200ffc3d3a7f30ad2719dc2a2a1f4fb
Asim <asim.232@gmail.com>
2020-02-13 22:20:18 -0600

    fizzbuzz implementation in dev

---
+++ FizzBuzzDemo.java
@@ -0,0 +1,19 @@
+/*
+* FizzBizz Implementation in Branch Dev
+*/
+class FizzBuzz{
+	
+	public static void main(String[] args) {
+		for (int var i=1; i < 101; i++){
+			if (i % (3 * 5) == 0) 
+				System.out.println("FizzBuzz");
+			else if (i % 3 == 0)
+				System.out.println("Fizz");
+			else if (i % 5 == 0)
+				System.out.println("Buzz");
+		else
+			System.out.println(i);
+		}
+
+	}
+}
